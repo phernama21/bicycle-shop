2ecd7ad408fc57eb4427263c59ba8ac16f10a418
phernama21 <pau.hm8@gmail.com>
2025-03-31 19:01:37 +0200

    Devise token auth implementation first step

--- api-backend/app/controllers/application_controller.rb
+++ api-backend/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+        include DeviseTokenAuth::Concerns::SetUserByToken
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  skip_forgery_protection
   allow_browser versions: :modern
 end
